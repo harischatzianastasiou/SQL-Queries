0c7af3f3e964b8eae0f40e50c05ed1f2a002470a
tchatzian <75744098+tchatzian@users.noreply.github.com>
2022-09-30 13:06:33 +0300

    Update cte.sql

--- Group Functions & CTE/cte.sql
+++ Group Functions & CTE/cte.sql
@@ -1,33 +1,20 @@
 -- Find all the employee whose salary is more than the average salary of all employees
 
-select avg(salary) from employees;
-
-select e.employee_id,a.avg_sal,count()
+--WITHOUT CTE
+select count(*) 
 from employees e,
 (   select avg(salary) as avg_sal
     from employees
 ) a
-GROUP BY e.employee_id,a.avg_sal
 where e.salary > a.avg_sal;
 
+--WITH CTE 
 
+WITH emps as (
+    SELECT salary, avg(salary) OVER() avg_sal
+    FROM employees
+)
+SELECT count(*)
+FROM emps
+WHERE emps.salary > emps.avg_sal;
 
-
-WITH dept_costs as (
-
-    SELECT e.department_id, sum(e.salary) dept_total, count(e.employee_id) empcnt
-
-    FROM hr.employees e
-
-    WHERE 1=1
-
-    GROUP BY e.department_id
-
-),
-
-avg_costs as(  SELECT department_id, trunc(dept_total/empcnt) avg_sal
-
-    FROM dept_costs )
-
-select avg(avg_costs.avg_sal)
-from avg_costs;

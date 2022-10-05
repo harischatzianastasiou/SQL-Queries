9783fcb987500899b636598f633b5ca690165509
tchatzian <75744098+tchatzian@users.noreply.github.com>
2022-10-05 16:15:06 +0300

    Create udemy The Advanced SQL Server Masterclass For Data Analysis.sql

---
+++ Analytic Functions/udemy The Advanced SQL Server Masterclass For Data Analysis.sql
@@ -0,0 +1,7 @@
+SELECT SUM(TotalDue) 
+FROM AdventureWorks2019.Sales.SalesOrderHeader
+
+SELECT TotalSales = SUM(TotalDue),PersonSalesID 
+FROM AdventureWorks2019.Sales.SalesOrderHeader
+GROUP BY PersonSalesID;
+

fca46a0857da9318e2678c6e7809b1df1b61db21
tchatzian <75744098+tchatzian@users.noreply.github.com>
2022-10-05 16:13:19 +0300

    Delete Window Functions directory

--- Window Functions/windowfunctions.sql
+++
@@ -1,6 +0,0 @@
-SELECT SUM(TotalDue) 
-FROM AdventureWorks2019.Sales.SalesOrderHeader
-
-SELECT TotalSales = SUM(TotalDue), SalesPersonID 
-FROM AdventureWorks2019.Sales.SalesOrderHeader
-GROUP BY SalesPersonID;

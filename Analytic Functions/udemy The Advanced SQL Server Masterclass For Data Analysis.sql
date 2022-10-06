56bf6e1b0e5330c16b023a5c245b9a21e1d84b8e
tchatzian <75744098+tchatzian@users.noreply.github.com>
2022-10-06 10:31:19 +0300

    Update udemy The Advanced SQL Server Masterclass For Data Analysis.sql

--- Analytic Functions/udemy The Advanced SQL Server Masterclass For Data Analysis.sql
+++ Analytic Functions/udemy The Advanced SQL Server Masterclass For Data Analysis.sql
@@ -1,8 +1,16 @@
-SELECT SUM(TotalDue) 
-FROM AdventureWorks2019.Sales.SalesOrderHeader
+--1. 
+SELECT 
+ B.FirstName,
+ B.LastName,
+ C.JobTitle,
+ A.Rate,
+ AVG(A.Rate) OVER() AverageRate,
+ MAX(A.Rate) OVER() MaximumRate,
+ A.Rate - AVG(A.Rate) OVER() DiffFromAverageRate,
+ A.Rate / MAX(A.Rate) OVER() * 100 PercentofMaxRate
 
-SELECT TotalSales = SUM(TotalDue),PersonSalesID 
-FROM AdventureWorks2019.Sales.SalesOrderHeader
-GROUP BY PersonSalesID;
-
- 
+FROM AdventureWorks2019.HumanResources.EmployeePayHistory A
+	JOIN AdventureWorks2019.Person.Person B
+		ON A.BusinessEntityID = B.BusinessEntityID
+	JOIN AdventureWorks2019.HumanResources.Employee C
+		ON A.BusinessEntityID = C.BusinessEntityID

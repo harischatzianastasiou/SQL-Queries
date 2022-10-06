bb623db886248f1e7204d1e6f5f904e360e44f9b
tchatzian <75744098+tchatzian@users.noreply.github.com>
2022-10-06 11:51:41 +0300

    Update udemy The Advanced SQL Server Masterclass For Data Analysis.sql

--- Analytic Functions/udemy The Advanced SQL Server Masterclass For Data Analysis.sql
+++ Analytic Functions/udemy The Advanced SQL Server Masterclass For Data Analysis.sql
@@ -14,3 +14,19 @@ FROM AdventureWorks2019.HumanResources.EmployeePayHistory A
 		ON A.BusinessEntityID = B.BusinessEntityID
 	JOIN AdventureWorks2019.HumanResources.Employee C
 		ON A.BusinessEntityID = C.BusinessEntityID
+
+--2.
+SELECT 
+A.Name ProductName,
+A.ListPrice,
+B.Name ProductSubcategory,
+C.Name ProductCategory,
+avg(A.ListPrice) OVER(PARTITION BY C.Name) AvgPriceByCategory,
+avg(A.ListPrice) OVER(PARTITION BY C.Name,B.NAME) AvgPriceByCategoryAndSubcategory,
+A.ListPrice - avg(A.ListPrice) OVER(PARTITION BY C.Name) ProductVsCategoryDelta
+
+FROM AdventureWorks2019.Production.Product A
+	INNER JOIN AdventureWorks2019.Production.ProductSubcategory B 
+		ON A.ProductSubcategoryID = B.ProductSubcategoryID
+	INNER JOIN AdventureWorks2019.Production.ProductCategory C 
+		ON C.ProductCategoryID = B.ProductCategoryID;
